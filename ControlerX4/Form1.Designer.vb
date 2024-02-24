1fdd7ce7b6819fa285335bfa84b1ef72a768ff97
masaya ishihara <vf2_casino@yahoo.co.jp>
2024-02-24 17:51:05 +0900

    プロジェクト ファイルを追加します。

---
+++ ControlerX4/Form1.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.stb1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.jumpbox1 = New System.Windows.Forms.PictureBox()
+        Me.beatbox1 = New System.Windows.Forms.PictureBox()
+        Me.chargebox1 = New System.Windows.Forms.PictureBox()
+        Me.shiftbox1 = New System.Windows.Forms.PictureBox()
+        Me.joybox1 = New System.Windows.Forms.PictureBox()
+        Me.Surround = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.tt1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Pict_S = New System.Windows.Forms.PictureBox()
+        Me.Pict_B = New System.Windows.Forms.PictureBox()
+        Me.Pict_C = New System.Windows.Forms.PictureBox()
+        Me.Pict_J = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.stb1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.jumpbox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.beatbox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.chargebox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.shiftbox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.joybox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pict_S, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pict_B, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pict_C, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Pict_J, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.Color.DimGray
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.Font = New System.Drawing.Font("Meiryo UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.RichTextBox1.ForeColor = System.Drawing.Color.White
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox1.Size = New System.Drawing.Size(140, 954)
+        Me.RichTextBox1.TabIndex = 58
+        Me.RichTextBox1.Text = ""
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.stb1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.jumpbox1)
+        Me.Panel1.Controls.Add(Me.beatbox1)
+        Me.Panel1.Controls.Add(Me.chargebox1)
+        Me.Panel1.Controls.Add(Me.shiftbox1)
+        Me.Panel1.Location = New System.Drawing.Point(98, 985)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(110, 65)
+        Me.Panel1.TabIndex = 57
+        '
+        'stb1
+        '
+        Me.stb1.BackgroundImage = CType(resources.GetObject("stb1.BackgroundImage"), System.Drawing.Image)
+        Me.stb1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.stb1.ErrorImage = CType(resources.GetObject("stb1.ErrorImage"), System.Drawing.Image)
+        Me.stb1.InitialImage = Nothing
+        Me.stb1.Location = New System.Drawing.Point(120, 47)
+        Me.stb1.Name = "stb1"
+        Me.stb1.Size = New System.Drawing.Size(10, 10)
+        Me.stb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.stb1.TabIndex = 55
+        Me.stb1.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(5, -45)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(39, 39)
+        Me.PictureBox1.TabIndex = 36
+        Me.PictureBox1.TabStop = False
+        '
+        'jumpbox1
+        '
+        Me.jumpbox1.BackgroundImage = CType(resources.GetObject("jumpbox1.BackgroundImage"), System.Drawing.Image)
+        Me.jumpbox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.jumpbox1.ErrorImage = CType(resources.GetObject("jumpbox1.ErrorImage"), System.Drawing.Image)
+        Me.jumpbox1.InitialImage = Nothing
+        Me.jumpbox1.Location = New System.Drawing.Point(89, 21)
+        Me.jumpbox1.Name = "jumpbox1"
+        Me.jumpbox1.Size = New System.Drawing.Size(16, 16)
+        Me.jumpbox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.jumpbox1.TabIndex = 35
+        Me.jumpbox1.TabStop = False
+        '
+        'beatbox1
+        '
+        Me.beatbox1.BackgroundImage = CType(resources.GetObject("beatbox1.BackgroundImage"), System.Drawing.Image)
+        Me.beatbox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.beatbox1.InitialImage = Nothing
+        Me.beatbox1.Location = New System.Drawing.Point(54, 24)
+        Me.beatbox1.Name = "beatbox1"
+        Me.beatbox1.Size = New System.Drawing.Size(16, 16)
+        Me.beatbox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.beatbox1.TabIndex = 33
+        Me.beatbox1.TabStop = False
+        '
+        'chargebox1
+        '
+        Me.chargebox1.BackgroundImage = CType(resources.GetObject("chargebox1.BackgroundImage"), System.Drawing.Image)
+        Me.chargebox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.chargebox1.InitialImage = Nothing
+        Me.chargebox1.Location = New System.Drawing.Point(71, 16)
+        Me.chargebox1.Name = "chargebox1"
+        Me.chargebox1.Size = New System.Drawing.Size(16, 16)
+        Me.chargebox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.chargebox1.TabIndex = 34
+        Me.chargebox1.TabStop = False
+        '
+        'shiftbox1
+        '
+        Me.shiftbox1.BackgroundImage = CType(resources.GetObject("shiftbox1.BackgroundImage"), System.Drawing.Image)
+        Me.shiftbox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.shiftbox1.InitialImage = Nothing
+        Me.shiftbox1.Location = New System.Drawing.Point(49, 41)
+        Me.shiftbox1.Name = "shiftbox1"
+        Me.shiftbox1.Size = New System.Drawing.Size(16, 16)
+        Me.shiftbox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.shiftbox1.TabIndex = 32
+        Me.shiftbox1.TabStop = False
+        '
+        'joybox1
+        '
+        Me.joybox1.BackColor = System.Drawing.Color.Transparent
+        Me.joybox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.joybox1.Location = New System.Drawing.Point(12, 951)
+        Me.joybox1.Name = "joybox1"
+        Me.joybox1.Size = New System.Drawing.Size(96, 96)
+        Me.joybox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.joybox1.TabIndex = 31
+        Me.joybox1.TabStop = False
+        '
+        'Surround
+        '
+        Me.Surround.Enabled = True
+        Me.Surround.Interval = 1
+        '
+        'tt1
+        '
+        Me.tt1.Interval = 1000
+        '
+        'Pict_S
+        '
+        Me.Pict_S.Location = New System.Drawing.Point(144, 500)
+        Me.Pict_S.Name = "Pict_S"
+        Me.Pict_S.Size = New System.Drawing.Size(6, 500)
+        Me.Pict_S.TabIndex = 75
+        Me.Pict_S.TabStop = False
+        '
+        'Pict_B
+        '
+        Me.Pict_B.Location = New System.Drawing.Point(160, 500)
+        Me.Pict_B.Name = "Pict_B"
+        Me.Pict_B.Size = New System.Drawing.Size(6, 500)
+        Me.Pict_B.TabIndex = 74
+        Me.Pict_B.TabStop = False
+        '
+        'Pict_C
+        '
+        Me.Pict_C.Location = New System.Drawing.Point(176, 500)
+        Me.Pict_C.Name = "Pict_C"
+        Me.Pict_C.Size = New System.Drawing.Size(6, 500)
+        Me.Pict_C.TabIndex = 73
+        Me.Pict_C.TabStop = False
+        '
+        'Pict_J
+        '
+        Me.Pict_J.Location = New System.Drawing.Point(192, 500)
+        Me.Pict_J.Name = "Pict_J"
+        Me.Pict_J.Size = New System.Drawing.Size(6, 500)
+        Me.Pict_J.TabIndex = 72
+        Me.Pict_J.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DimGray
+        Me.ClientSize = New System.Drawing.Size(216, 1050)
+        Me.Controls.Add(Me.Pict_S)
+        Me.Controls.Add(Me.Pict_B)
+        Me.Controls.Add(Me.Pict_C)
+        Me.Controls.Add(Me.Pict_J)
+        Me.Controls.Add(Me.joybox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Meiryo UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ControlerX4"
+        Me.TransparencyKey = System.Drawing.Color.DimGray
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.stb1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.jumpbox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.beatbox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.chargebox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.shiftbox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.joybox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pict_S, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pict_B, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pict_C, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Pict_J, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents stb1 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents joybox1 As PictureBox
+    Friend WithEvents jumpbox1 As PictureBox
+    Friend WithEvents beatbox1 As PictureBox
+    Friend WithEvents chargebox1 As PictureBox
+    Friend WithEvents shiftbox1 As PictureBox
+    Friend WithEvents Surround As Timer
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents tt1 As Timer
+    Friend WithEvents Pict_S As PictureBox
+    Friend WithEvents Pict_B As PictureBox
+    Friend WithEvents Pict_C As PictureBox
+    Friend WithEvents Pict_J As PictureBox
+End Class
---
+++ ControlerX4/Form1.vb
@@ -0,0 +1,387 @@
+﻿Imports SharpDX.XInput
+
+
+Public Class Form1
+
+    Dim Panb(100) As Panel
+    Dim Panc(100) As Panel
+    Dim Panj(100) As Panel
+    Dim Pans(100) As Panel
+
+    Dim Panb_i As Integer = 0
+    Dim Panc_i As Integer = 0
+    Dim Panj_i As Integer = 0
+    Dim Pans_i As Integer = 0
+    Dim Panb_f As Boolean = False
+    Dim Panc_f As Boolean = False
+    Dim Panj_f As Boolean = False
+    Dim Pans_f As Boolean = False
+
+    Dim target_b As Control = Nothing
+    Dim target_c As Control = Nothing
+    Dim target_j As Control = Nothing
+    Dim target_s As Control = Nothing
+
+    Dim ww As Integer = 6
+    Dim nn As Integer = 2
+    Dim endn As Integer = 490
+
+    Dim lastLine_bin As String = ""
+    'Dim tfmax = 180
+    'Dim tfi = 5000
+    'Dim em2len = 3000
+    Dim Center_i As Integer = 0
+    Dim siz As Integer = 16
+    Dim etime1 As Integer = 0
+    Dim Lvr_bin As String = "0000"
+    Dim butt_bin As String = "9999"
+    Dim buttOn As Boolean = False
+    Dim comnd1 As Boolean = True
+    Dim comnd2 As Boolean = True
+    Dim mode = 3
+
+    Private mousePoint As Point
+
+    Private Sub Form1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles jumpbox1.MouseDown
+        If (e.Button And MouseButtons.Left) = MouseButtons.Left Then
+            mousePoint = New Point(e.X, e.Y)
+        End If
+    End Sub
+
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles jumpbox1.MouseMove
+        If (e.Button And MouseButtons.Left) = MouseButtons.Left Then
+            Me.Left += e.X - mousePoint.X
+            Me.Top += e.Y - mousePoint.Y
+        End If
+    End Sub
+    Private Sub SurroundingSub1()
+
+        Dim controller = New SharpDX.XInput.Controller(SharpDX.XInput.UserIndex.One)
+        If controller.IsConnected Then
+            Dim state = controller.GetState()
+            Dim a = state.Gamepad.Buttons
+
+            Dim n As String = TentoTwo(a).PadLeft(16, "0")
+            'TextBox1.Text = n
+            Dim lever As String = Strings.Right(n, 4)
+            Dim n1 = "0"
+            Dim n2 = "0"
+            Dim n3 = "0"
+            Dim n4 = "0"
+            Dim x = state.Gamepad.LeftThumbX
+            Dim y = state.Gamepad.LeftThumbY
+
+            If (y <> Center_i Or x <> Center_i) And lever = "0000" Then
+                If y > Center_i Then
+                    n1 = "1"
+                End If
+                If y < Center_i Then
+                    n2 = "1"
+                End If
+                If x < Center_i Then
+                    n3 = "1"
+                End If
+                If x > Center_i Then
+                    n4 = "1"
+                End If
+                lever = n4 & n3 & n2 & n1
+            End If
+
+            joybox1.Image = My.Resources.ResourceManager.GetObject("_" & lever.ToString)
+
+            'Shift
+            Dim ss As String = n.Substring(3, 1)
+            If ss = "0" Then
+                shiftbox1.Image = Nothing
+                If comnd2 = True Then
+                    target_s = Nothing
+                    Pans_f = False
+                End If
+            Else
+                shiftbox1.Image = My.Resources.sd
+                If comnd2 = True Then
+                    If Pans_f = False Then
+                        Pans_f = True
+                        Pans(0) = New Panel With {
+                                   .Width = ww,
+                                   .Height = nn,
+                                   .Name = Pans_i.ToString,
+                                   .Location = New System.Drawing.Point(0, endn),
+                                   .BackColor = Color.FromArgb(192, 40, 247)
+                               }
+                        Me.Pict_S.Controls.Add(Pans(0))
+                    End If
+                End If
+            End If
+            For Each cs As Control In Me.Pict_S.Controls
+                If Pans_f = True And cs.Name = "0".ToString Then
+                    target_s = cs
+                    target_s.Height += nn
+                Else
+                    cs.Name = "1"
+                End If
+                If cs.Top <= 0 - cs.Height Then
+                    Me.Pict_S.Controls.Remove(cs)
+                Else
+                    cs.Top -= nn
+                End If
+            Next
+
+            'Beat
+            Dim bb As String = n.Substring(1, 1)
+            If bb = "0" Then
+                beatbox1.Image = Nothing
+                If comnd2 = True Then
+                    target_b = Nothing
+                    Panb_f = False
+                End If
+            Else
+                beatbox1.Image = My.Resources.bd
+                If comnd2 = True Then
+                    If Panb_f = False Then
+                        Panb_f = True
+                        Panb(0) = New Panel With {
+                                       .Width = ww,
+                                       .Height = nn,
+                                       .Name = Panb_i.ToString,
+                                       .Location = New System.Drawing.Point(0, endn),
+                                       .BackColor = Color.FromArgb(255, 128, 0)
+                                   }
+                        Me.Pict_B.Controls.Add(Panb(0))
+                    End If
+                End If
+            End If
+            For Each cb As Control In Me.Pict_B.Controls
+                If Panb_f = True And cb.Name = "0".ToString Then
+                    target_b = cb
+                    target_b.Height += nn
+                Else
+                    cb.Name = "1"
+                End If
+                If cb.Top <= 0 - cb.Height Then
+                    Me.Pict_B.Controls.Remove(cb)
+                Else
+                    cb.Top -= nn
+                End If
+            Next
+
+            'Charge
+            Dim cc As String = n.Substring(0, 1)
+            If cc = "0" Then
+                chargebox1.Image = Nothing
+                If comnd2 = True Then
+                    target_c = Nothing
+                    Panc_f = False
+                End If
+            Else
+                chargebox1.Image = My.Resources.cd
+                If comnd2 = True Then
+                    If Panc_f = False Then
+                        Panc_f = True
+                        Panc(0) = New Panel With {
+                                       .Width = ww,
+                                       .Height = nn,
+                                       .Name = Panc_i.ToString,
+                                       .Location = New System.Drawing.Point(0, endn),
+                                       .BackColor = Color.Red
+                                   }
+                        Me.Pict_C.Controls.Add(Panc(0))
+                    End If
+                End If
+            End If
+            For Each cch As Control In Me.Pict_C.Controls
+                If Panc_f = True And cch.Name = "0".ToString Then
+                    target_c = cch
+                    target_c.Height += nn
+                Else
+                    cch.Name = "1"
+                End If
+                If cch.Top <= 0 - cch.Height Then
+                    Me.Pict_C.Controls.Remove(cch)
+                Else
+                    cch.Top -= nn
+                End If
+            Next
+
+            'Jump
+            Dim jj As String = n.Substring(6, 1)
+            If jj = "0" Then
+                jumpbox1.Image = Nothing
+                If comnd2 = True Then
+                    target_j = Nothing
+                    Panj_f = False
+                End If
+            Else
+                jumpbox1.Image = My.Resources.jd
+                If comnd2 = True Then
+                    If Panj_f = False Then
+                        Panj_f = True
+                        Panj(0) = New Panel With {
+                                       .Width = ww,
+                                       .Height = nn,
+                                       .Name = Panj_i.ToString,
+                                       .Location = New System.Drawing.Point(0, endn),
+                                       .BackColor = Color.Yellow
+                                   }
+                        Me.Pict_J.Controls.Add(Panj(0))
+                    End If
+                End If
+            End If
+
+            For Each cj As Control In Me.Pict_J.Controls
+                    If Panj_f = True And cj.Name = "0".ToString Then
+                        target_j = cj
+                        target_j.Height += nn
+                    Else
+                        cj.Name = "1"
+                    End If
+                    If cj.Top <= 0 - cj.Height Then
+                        Me.Pict_J.Controls.Remove(cj)
+                    Else
+                        cj.Top -= nn
+                    End If
+                Next
+
+
+                'Dim sl As String = n.Substring(10, 1)
+                'If sl = "1" And slf1 = 0 Then
+                '    slf1 = 1
+                'ElseIf sl = 0 Then
+                '    slf1 = 0
+                'End If
+
+                If comnd1 = True Then
+                etime1 += 1
+                Dim buttons As String = "b" & ss & bb & cc & jj
+                If (buttons <> "0000" And buttons <> butt_bin.ToString) Then
+                    If etime1 > 99 Then
+                        etime1 = 0
+                        RichTextBox1.Text = ""
+                    End If
+                    Dim etime0 As String = etime1.ToString("000")
+                    RichTextBox1.AppendText(vbCrLf & etime0 & " : ")
+                    butt_bin = buttons
+                    '.Image = My.Resources.bd
+                    'Using fs As System.IO.FileStream = New System.IO.FileStream(My.Resources.ResourceManager.GetObject(buttons.ToString),
+                    'System.IO.FileMode.Open, System.IO.FileAccess.Read)
+                    'Dim bmp As Bitmap = New Bitmap(System.Drawing.Image.FromStream(fs))
+                    Dim bmpB As Bitmap = My.Resources.ResourceManager.GetObject(buttons.ToString)
+                    bmpB = New Bitmap(bmpB, CInt(siz * 4), CInt(siz))
+                    Clipboard.SetDataObject(bmpB, True)
+                    'End Using
+                    Dim iData2 As IDataObject = Clipboard.GetDataObject()
+                    If iData2.GetDataPresent(DataFormats.Bitmap) Then
+                        RichTextBox1.Paste()
+                    End If
+                    Lvr_bin = lever
+                    'Using fs As System.IO.FileStream = New System.IO.FileStream("include\l" & lever & ".png",
+                    'System.IO.FileMode.Open, System.IO.FileAccess.Read)
+                    'Dim bmp As Bitmap = New Bitmap(System.Drawing.Image.FromStream(fs))
+                    Dim bmpL As Bitmap = My.Resources.ResourceManager.GetObject("l" & lever.ToString)
+                    bmpL = New Bitmap(bmpL, CInt(siz), CInt(siz))
+                    Clipboard.SetDataObject(bmpL, True)
+                    'End Using
+                    Dim iData1 As IDataObject = Clipboard.GetDataObject()
+                    If iData1.GetDataPresent(DataFormats.Bitmap) Then
+                        RichTextBox1.Paste()
+                    End If
+                    RichTextBox1.ScrollToCaret()
+                    etime1 = 0
+                    buttOn = True
+                End If
+
+                If (lever <> Lvr_bin) Then
+                    If etime1 > 99 Then
+                        etime1 = 0
+                        RichTextBox1.Text = ""
+                    End If
+                    Dim etime0 As String = etime1.ToString("000")
+                    RichTextBox1.AppendText(vbCrLf & etime0 & " : ")
+                    'Using fs As System.IO.FileStream = New System.IO.FileStream("include\" & buttons & ".png",
+                    '                                              System.IO.FileMode.Open, System.IO.FileAccess.Read)
+                    '    Dim bmp As Bitmap = New Bitmap(System.Drawing.Image.FromStream(fs))
+                    Dim bmpB As Bitmap = My.Resources.ResourceManager.GetObject(buttons.ToString)
+                    bmpB = New Bitmap(bmpB, CInt(siz * 4), CInt(siz))
+                    Clipboard.SetDataObject(bmpB, True)
+                    'End Using
+                    Dim iData2 As IDataObject = Clipboard.GetDataObject()
+                    If iData2.GetDataPresent(DataFormats.Bitmap) Then
+                        RichTextBox1.Paste()
+                    End If
+                    Lvr_bin = lever
+                    Dim bmpL As Bitmap = My.Resources.ResourceManager.GetObject("l" & lever.ToString)
+                    bmpL = New Bitmap(bmpL, CInt(siz), CInt(siz))
+                    Clipboard.SetDataObject(bmpL, True)
+                    Dim iData1 As IDataObject = Clipboard.GetDataObject()
+                    If iData1.GetDataPresent(DataFormats.Bitmap) Then
+                        RichTextBox1.Paste()
+                    End If
+                    etime1 = 0
+                    RichTextBox1.ScrollToCaret()
+                End If
+            End If
+        Else
+            joybox1.Image = Nothing
+        End If
+    End Sub
+
+    Private Function TentoTwo(ByVal value As String) As String
+        If Math.Floor(value / 2) Then
+            Return TentoTwo(Math.Floor(value / 2)) + CStr(value Mod 2)
+        End If
+        Return CStr(value Mod 2)
+    End Function
+
+
+    Private Sub Surround_Tick(sender As Object, e As EventArgs) Handles Surround.Tick
+        SurroundingSub1()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.TransparencyKey = Me.BackColor
+        Me.TopMost = True
+    End Sub
+
+    Private Sub jumpbox1_Click(sender As Object, e As MouseEventArgs) Handles jumpbox1.DoubleClick
+        If Me.WindowState <> FormWindowState.Maximized Then
+            Me.WindowState = FormWindowState.Maximized
+        Else
+            Me.WindowState = FormWindowState.Normal
+        End If
+    End Sub
+
+    Private Sub shiftbox1_Click(sender As Object, e As EventArgs) Handles shiftbox1.DoubleClick
+        Me.Close()
+    End Sub
+
+    Private Sub beatbox1_Click(sender As Object, e As EventArgs) Handles beatbox1.Click
+        If Center_i = 0 Then
+            Center_i = 128
+        Else
+            Center_i = 0
+        End If
+    End Sub
+
+    Private Sub chargebox1_Click(sender As Object, e As EventArgs) Handles chargebox1.Click
+        Select Case mode
+            Case 3
+                comnd1 = False
+                comnd2 = False
+            Case 2
+                comnd1 = True
+                comnd2 = False
+            Case 1
+                comnd1 = False
+                comnd2 = True
+            Case 0
+                comnd1 = True
+                comnd2 = True
+        End Select
+        If mode > 0 Then
+            mode -= 1
+        Else
+            mode = 3
+        End If
+        RichTextBox1.Text = ""
+    End Sub
+End Class
